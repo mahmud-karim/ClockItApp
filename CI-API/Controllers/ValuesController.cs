747ca4df8e437243b788703c38450de18d9ee819
mahmud <mahmudskarim@gmail.com>
2020-08-02 02:10:21 -0400

    updated to .net 3.0 and fixed all bugs associated

--- CI-API/Controllers/ValuesController.cs
+++ CI-API/Controllers/ValuesController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CI_API.Controllers
+namespace CI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
