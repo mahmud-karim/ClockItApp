26857608dcfd6f2e4743e66006ce28fc5cb8ac6a
mahmud <mahmudskarim@gmail.com>
2020-08-20 18:09:06 -0400

    working on template driven forms

--- CI-SPA/src/app/auth/components/login/login.component.ts
+++ CI-SPA/src/app/auth/components/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(f: NgForm) {
+    console.log(f.value); // { first: '', last: '' }
+    console.log(f.valid); // false
   }
-
 }
